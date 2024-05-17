f90686f0a9f64dc731bf5281dbd1a201afc92f1c
Ian Gilmore <ian.gilmore@me.com>
2024-05-17 16:36:40 -0400

    Add Course, StudentEnrollment, CourseStaff, Assignment, Submission, and Attendance models, add URLs (commented out until Views are written)
    Co-authored-by: Addy Jaime <AddyJaime@users.noreply.github.com>

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path # type: ignore
-from .views import Home
+from django.urls import path
+# from .views import Home, LoginView, VerifyUserView, CoursesView, CreateCourseView, CourseView, CourseStaffView, StudentEnrollmentView, AssignmentsView, CreateAssignmentView, AssignmentView, StudentsView, StudentView
 
 urlpatterns = [
-  path('', Home.as_view(), name='home'),
-]
-
-# Ian edit
-# Addy edit 
+  # path('', Home.as_view(), name='home'),
+  # path('users/login/', LoginView.as_view(), name='login'),
+  # path('users/token/refresh/', VerifyUserView.as_view(), name='token_refresh'),
+  # path('courses/', CoursesView.as_view(), name='courses'),
+  # path('course/', CreateCourseView.as_view(), name='create_course'),
+  # path('<int:course_id>/', CourseView.as_view(), name='course'),
+  # path('<int:course_id>/staff/<int:user_id>/', CourseStaffView.as_view(), name='course_staff'),
+  # path('<int:course_id>/student-enrollment/<int:user_id>/', StudentEnrollmentView.as_view(), name='student_enrollment'),
+  # path('<int:course_id>/assignments/', AssignmentsView.as_view(), name='assignments'),
+  # path('<int:course_id>/assignment/', CreateAssignmentView.as_view(), name='create_assignment'),
+  # path('<int:course_id>/assignment/<int:assignment_id>/', AssignmentView.as_view(), name='assignment'),
+  # path('<int:course_id>/students/', StudentsView.as_view(), name='students'),
+  # path('<int:course_id>/student/<int:student_enrollment_id>/', StudentView.as_view(), name='student'),
+  ]
