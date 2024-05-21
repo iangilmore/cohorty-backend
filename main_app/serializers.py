7b9edc49c1cf172e02023dc1862aeae566d55c0a
Ian Gilmore <ian.gilmore@me.com>
2024-05-21 15:42:52 -0400

    nearly complete GET StudentDetails

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -37,7 +37,6 @@ class StudentSerializer(serializers.ModelSerializer):
     total = round(absences.count() * 1 + lates.count() * 0.333, 2)
     return total
 
-
 class AssignmentListSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Assignment
@@ -64,6 +63,7 @@ class AssignmentDetailSerializer(serializers.ModelSerializer):
     model = Assignment
     fields = ['id', 'name', 'due_date', 'submissions']
 
+
 class StudentEnrollmentSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StudentEnrollment
@@ -79,7 +79,28 @@ class CourseStaffSerializer(serializers.ModelSerializer):
 
 
 class AttendanceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Attendance
-        fields = '__all__'
-        read_only_fields = ['student', 'course']
+  student = serializers.StringRelatedField()
+  class Meta:
+    model = Attendance
+    fields = ['id', 'date', 'status', 'student']
+
+
+class StudentDetailSerializer(serializers.ModelSerializer):
+  name = serializers.SerializerMethodField()
+  submissions = serializers.SerializerMethodField()
+  attendance = serializers.SerializerMethodField()
+  
+  class Meta:
+    model = StudentEnrollment
+    fields = ['id', 'name', 'submissions', 'attendance']
+    
+  def get_name(self, obj):
+    return obj.name()
+  
+  def get_submissions(self, obj):
+    submissions = Submission.objects.filter(student=obj)
+    return SubmissionSerializer(submissions, many=True).data
+  
+  def get_attendance(self, obj):
+    attendance = Attendance.objects.filter(student=obj)
+    return AttendanceSerializer(attendance, many=True).data
