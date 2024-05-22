c16c6d4fc9e0af168ce1dd75b0658d15681c844a
Ian Gilmore <ian.gilmore@me.com>
2024-05-22 13:28:09 -0400

    Refactor views, serializers and URL naming conventions

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,18 +1,42 @@
 from django.urls import path
-from .views import Home, Login, VerifyUser, CourseList, CreateCourse, CourseDetail, CourseStaff, StudentEnrollmentView, AssignmentList, CreateAssignment, AssignmentDetail, StudentList,StudentDetail
+from .views import (
+    Home,
+    Login,
+    VerifyUser,
+    Courses,
+    CourseDetail,
+    Staff,
+    Assignments,
+    AssignmentDetail,
+    Students,
+    StudentDetail,
+)
 
 urlpatterns = [
-  path('', Home.as_view(), name='home'),
-  path('users/login/', Login.as_view(), name='login'),
-  path('users/token/refresh/', VerifyUser.as_view(), name='token_refresh'),
-  path('courses/', CourseList.as_view(), name='courses'),
-  path('course/', CreateCourse.as_view(), name='create_course'),
-  path('<int:id>/', CourseDetail.as_view(), name='course_detail'),
-  path('<int:course_id>/staff/<int:user_id>/', CourseStaff.as_view(), name='course_staff'),
-  path('<int:course_id>/student-enrollment/<int:user_id>/', StudentEnrollmentView.as_view(), name='student_enrollment'),
-  path('<int:id>/assignments/', AssignmentList.as_view(), name='assignments'),
-  path('<int:id>/assignment/', CreateAssignment.as_view(), name='create_assignment'),
-  path('<int:course_id>/assignment/<int:id>/', AssignmentDetail.as_view(), name='assignment'),
-  path('<int:id>/students/', StudentList.as_view(), name='students'),
-  path('<int:course_id>/student/<int:id>/', StudentDetail.as_view(), name='student'),
-  ]
+    path("", Home.as_view(), name="home"),
+    path("users/login/", Login.as_view(), name="login"),
+    path("users/token/refresh/", VerifyUser.as_view(), name="token_refresh"),
+    path("courses/", Courses.as_view(), name="courses"),
+    path("courses/<int:id>/", CourseDetail.as_view(), name="course_detail"),
+    path(
+        "courses/<int:course_id>/staff/<int:user_id>/",
+        Staff.as_view(),
+        name="course_staff",
+    ),
+    path(
+        "courses/<int:id>/assignments/",
+        Assignments.as_view(),
+        name="assignments",
+    ),
+    path(
+        "courses/<int:course_id>/assignments/<int:id>/",
+        AssignmentDetail.as_view(),
+        name="assignment",
+    ),
+    path("courses/<int:course_id>/students/", Students.as_view(), name="students"),
+    path(
+        "courses/<int:course_id>/students/<int:id>/",
+        StudentDetail.as_view(),
+        name="student",
+    ),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,229 +1,241 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, status, permissions
-from .models import Course, Assignment, CourseStaff, StudentEnrollment, Submission, Attendance
-from .serializers import CourseSerializer, AssignmentListSerializer, AssignmentDetailSerializer, CourseStaffSerializer, StudentEnrollmentSerializer, SubmissionSerializer, AttendanceSerializer, UserSerializer, StudentSerializer, StudentDetailSerializer
+from .models import (
+    Course,
+    Assignment,
+    CourseStaff,
+    StudentEnrollment,
+    Submission,
+    Attendance,
+)
+from .serializers import (
+    UserSerializer,
+    CourseSerializer,
+    CourseStaffSerializer,
+    AssignmentListSerializer,
+    AssignmentDetailSerializer,
+    SubmissionSerializer,
+    AttendanceSerializer,
+    StudentSerializer,
+    StudentDetailSerializer,
+)
+
 # from .serializers import UserSerializerWithToken, CourseListSerializer, CourseDetailSerializer, StudentListSerializer
 from django.contrib.auth import authenticate
 from rest_framework.exceptions import PermissionDenied
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
+
+
 # Define the home vie
 class Home(APIView):
-	def get(self, request):
-		content = {'message': 'Welcome to the Cohorty API home route!'}
-		return Response(content)
+    def get(self, request):
+        content = {"message": "Welcome to the Cohorty API home route!"}
+        return Response(content)
 
 
 # Define the login view
 class Login(APIView):
-	permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny]
 
-	def post(self, request):
-		username = request.data.get('username')
-		password = request.data.get('password')
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
 
-		if username is None or password is None:
-			return Response({'error': 'Please provide both username and password'}, 
-                status=status.HTTP_400_BAD_REQUEST
-                )
+        if username is None or password is None:
+            return Response(
+                {"error": "Please provide both username and password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-		user = authenticate(username=username, password=password)
+        user = authenticate(username=username, password=password)
 
-		if not user:
-			return Response({'error': 'Invalid Credentials'},
-                status=status.HTTP_404_NOT_FOUND
-                )
+        if not user:
+            return Response(
+                {"error": "Invalid Credentials"}, status=status.HTTP_404_NOT_FOUND
+            )
 
-		refresh = RefreshToken.for_user(user)
-		return Response({
-			'refresh': str(refresh),
-			'access': str(refresh.access_token),
-			'user': UserSerializer(user).data
-		}, status=status.HTTP_200_OK)
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "user": UserSerializer(user).data,
+            },
+            status=status.HTTP_200_OK,
+        )
 
 
-#VerifyUserView
+# VerifyUserView
 class VerifyUser(APIView):
-	#This ensures that only authenticated users can access this view.
-	permission_classes = [permissions.IsAuthenticated]
-	#request: Represents the HTTP request received
-	def get(self, request):
-	#Searches the database for the User object whose username matches request.user
-		user = User.objects.get(username=request.user)
-		refresh = RefreshToken.for_user(request.user)
-		return Response({
-			'refresh': str(refresh),
-			'access': str(refresh.access_token),
-			'user': UserSerializer(user).data
-			})
+    # This ensures that only authenticated users can access this view.
+    permission_classes = [permissions.IsAuthenticated]
+
+    # request: Represents the HTTP request received
+    def get(self, request):
+        # Searches the database for the User object whose username matches request.user
+        user = User.objects.get(username=request.user)
+        refresh = RefreshToken.for_user(request.user)
+        return Response(
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "user": UserSerializer(user).data,
+            }
+        )
 
 
-#CoursesView
-# think we do not need one list and create because list = list and create = create so they do both, they inhere both, get and post 
-class CourseList(generics.ListCreateAPIView):
-	# permission_classes = [permissions.IsAuthenticated]
-	queryset = Course.objects.all()
-	serializer_class = CourseSerializer
+# CoursesView
+# think we do not need one list and create because list = list and create = create so they do both, they inhere both, get and post
+class Courses(generics.ListCreateAPIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
 
-#CreateCourseView
-class CreateCourse(generics.CreateAPIView):
-	# permission_classes = [permissions.IsAuthenticated]
-	queryset = Course.objects.all()
-	serializer_class = CourseSerializer
-
-
-#CourseView
+# CourseView
 class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
-	# permission_classes = [permissions.IsAuthenticated]
-	queryset = Course.objects.all()
-	serializer_class = CourseSerializer
-	lookup_field = 'id'
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    lookup_field = "id"
 
-	# def get_queryset(self):
-	# 	return Course.objects.filter(id=self.kwargs['course_id'])
-	
-	# def retrieve(self, request, *args, **kwargs):
-	# 	instance = self.get_object()
-	# 	serializer = self.get_serializer(instance)
-	# 	return Response(serializer.data)
+    # def get_queryset(self):
+    # 	return Course.objects.filter(id=self.kwargs['course_id'])
 
-
-#CourseStaffView
-class CourseStaff(generics.ListCreateAPIView):
-	# permission_classes = [permissions.IsAuthenticated]
-	queryset = Course.objects.all()
-	serializer_class = CourseStaffSerializer
+    # def retrieve(self, request, *args, **kwargs):
+    # 	instance = self.get_object()
+    # 	serializer = self.get_serializer(instance)
+    # 	return Response(serializer.data)
 
 
-#StudentEnrollmentView
-class StudentEnrollmentView(generics.ListCreateAPIView):
-	# permission_classes = [permissions.IsAuthenticated]
-	queryset = StudentEnrollment.objects.all()
-	serializer_class = StudentEnrollmentSerializer
-	lookup_field = 'id'
-
-	#TODO Needs Testing
-	def perform_create(self, serializer):
-		serializer.save(course_id=self.kwargs['id'], user_id=self.kwargs['user_id'])
+# CourseStaffView
+class Staff(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = CourseStaff.objects.all()
+    serializer_class = CourseStaffSerializer
 
 
-#AssignmentsView
-class AssignmentList(generics.ListCreateAPIView):
-  # permission_classes = [permissions.IsAuthenticated]
-  queryset = Assignment.objects.all()
-  serializer_class = AssignmentListSerializer
-  lookup_field = 'id'
+# AssignmentsView
+class Assignments(generics.ListCreateAPIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    # queryset = Assignment.objects.all()
+    serializer_class = AssignmentListSerializer
+    lookup_field = "id"
+
+    def get_queryset(self):
+        return Assignment.objects.filter(course_id=self.kwargs["id"])
 
 
-#CreateAssignmentView
-class CreateAssignment(generics.CreateAPIView):
-	# permission_classes = [permissions.IsAuthenticated]
-	serializer_class = AssignmentListSerializer
-
-
-#AssignmentView
+# AssignmentView
 class AssignmentDetail(generics.RetrieveUpdateDestroyAPIView):
-	# permission_classes = [permissions.IsAuthenticated]
-	queryset = Assignment.objects.all()
-	serializer_class = AssignmentDetailSerializer
-	lookup_field = 'id'
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentDetailSerializer
+    lookup_field = "id"
 
-	def put(self, request, *args, **kwargs):
-		assignment = self.get_object()
-		submissions_data = request.data.get('submissions', [])
+    def put(self, request, *args, **kwargs):
+        assignment = self.get_object()
+        submissions_data = request.data.get("submissions", [])
 
-		existing_submissions = Submission.objects.filter(assignment=assignment)
+        existing_submissions = Submission.objects.filter(assignment=assignment)
 
-		submission_ids = set(submission_data.get('id') for submission_data in submissions_data)
+        submission_ids = set(
+            submission_data.get("id") for submission_data in submissions_data
+        )
 
-		for submission in existing_submissions:
-			if submission.id not in submission_ids:
-				submission.delete()
+        for submission in existing_submissions:
+            if submission.id not in submission_ids:
+                submission.delete()
 
-		for submission_data in submissions_data:
-			submission_id = submission_data.get('id')
+        for submission_data in submissions_data:
+            submission_id = submission_data.get("id")
 
-			if submission_id:
-				submission = Submission.objects.filter(id=submission_id).first()
+            if submission_id:
+                submission = Submission.objects.filter(id=submission_id).first()
 
-				if submission:
-					serializer = SubmissionSerializer(submission, data=submission_data)
+                if submission:
+                    serializer = SubmissionSerializer(submission, data=submission_data)
 
-					if serializer.is_valid():
-						serializer.save()
-			else:
-				serializer = SubmissionSerializer(data=submission_data)
+                    if serializer.is_valid():
+                        serializer.save()
+            else:
+                serializer = SubmissionSerializer(data=submission_data)
 
-				if serializer.is_valid():
-					serializer.save(assignment=assignment)
+                if serializer.is_valid():
+                    serializer.save(assignment=assignment)
 
-		return self.update(request, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
 
-#StudentsView 
-class StudentList(generics.ListCreateAPIView):
-  # permission_classes = [permissions.IsAuthenticated]
-  serializer_class = StudentSerializer
-  lookup_field = 'id'
-  
-  def get_queryset(self):
-    return StudentEnrollment.objects.filter(course_id=self.kwargs['id'])
 
-#Student Detail
+# StudentsView
+class Students(generics.ListCreateAPIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        return StudentEnrollment.objects.filter(course_id=self.kwargs["course_id"])
+
+
+# Student Detail
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
-		# permission_classes = [permissions.IsAuthenticated]
-		queryset = StudentEnrollment.objects.all()
-		serializer_class = StudentDetailSerializer
-		lookup_field = 'id'
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = StudentEnrollment.objects.all()
+    serializer_class = StudentDetailSerializer
+    lookup_field = "id"
 
-		def put(self, request, *args, **kwargs):
-				student = self.get_object()
-				attendance_data = request.data.get('attendance', [])
-				submissions_data = request.data.get('submissions', [])
-				
-				existing_attendances = Attendance.objects.filter(student=student)
-				existing_submissions = Submission.objects.filter(student=student)
-				
-				attendance_ids = set(attendance_data.get('id') for attendance_data in attendance_data)
-				submission_ids = set(submission_data.get('id') for submission_data in submissions_data)
-				
-				for attendance in existing_attendances:
-						if attendance.id not in attendance_ids:
-								attendance.delete()
+    def put(self, request, *args, **kwargs):
+        student = self.get_object()
+        attendance_data = request.data.get("attendance", [])
+        submissions_data = request.data.get("submissions", [])
 
-				for submission in existing_submissions:
-						if submission.id not in submission_ids:
-								submission.delete()
+        existing_attendances = Attendance.objects.filter(student=student)
+        existing_submissions = Submission.objects.filter(student=student)
 
-				for attendance_data in attendance_data:
-						attendance_id = attendance_data.get('id')
-						if attendance_id:
-								attendance = Attendance.objects.filter(id=attendance_id).first()
-								if attendance:
-										serializer = AttendanceSerializer(attendance, data=attendance_data)
+        attendance_ids = set(
+            attendance_data.get("id") for attendance_data in attendance_data
+        )
+        submission_ids = set(
+            submission_data.get("id") for submission_data in submissions_data
+        )
 
-										if serializer.is_valid():
-												serializer.save()
-						else:
-								serializer = AttendanceSerializer(data=attendance_data)
+        for attendance in existing_attendances:
+            if attendance.id not in attendance_ids:
+                attendance.delete()
 
-								if serializer.is_valid():
-										serializer.save(student=student)
+        for submission in existing_submissions:
+            if submission.id not in submission_ids:
+                submission.delete()
 
-				for submission_data in submissions_data:
-						submission_id = submission_data.get('id')
-						if submission_id:
-								submission = Submission.objects.filter(id=submission_id).first()
-								if submission:
-										serializer = SubmissionSerializer(submission, data=submission_data)
+        for attendance_data in attendance_data:
+            attendance_id = attendance_data.get("id")
+            if attendance_id:
+                attendance = Attendance.objects.filter(id=attendance_id).first()
+                if attendance:
+                    serializer = AttendanceSerializer(attendance, data=attendance_data)
 
-										if serializer.is_valid():
-												serializer.save()
-						else:
-								serializer = SubmissionSerializer(data=submission_data)
+                    if serializer.is_valid():
+                        serializer.save()
+            else:
+                serializer = AttendanceSerializer(data=attendance_data)
 
-								if serializer.is_valid():
-										serializer.save(student=student)
-				
-				return self.update(request, *args, **kwargs)
+                if serializer.is_valid():
+                    serializer.save(student=student)
+
+        for submission_data in submissions_data:
+            submission_id = submission_data.get("id")
+            if submission_id:
+                submission = Submission.objects.filter(id=submission_id).first()
+                if submission:
+                    serializer = SubmissionSerializer(submission, data=submission_data)
+
+                    if serializer.is_valid():
+                        serializer.save()
+            else:
+                serializer = SubmissionSerializer(data=submission_data)
+
+                if serializer.is_valid():
+                    serializer.save(student=student)
+
+        return self.update(request, *args, **kwargs)
